5de918df1716c0a3928a097862939809a530270e
Sander Shvedov <sunundershadow@gmail.com>
2025-10-31 12:08:59 +0300

    removed tmp test file

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$str = 'аб';
-echo ord($str[0]), ' ', ord($str[1]);
-echo ord($str[2]), ' ', ord($str[3]);
-$first_char = mb_substr($str, 5 ,  1, "UTF-8"); // Extracts the first character
-
-echo IntlChar::isupper('Т') ? 'da' : 'net';
